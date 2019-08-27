9696b44cf0f90eb3aaea7f696507d6917346e2fa
Husni <mohhusnimubaraq@gmail.com>
2019-08-27 17:54:03 +0700

    deleted some file

--- app/src/main/java/id/husni/moviestvcatalogue/utilities/AppUtilities.java
+++ app/src/main/java/id/husni/moviestvcatalogue/utilities/AppUtilities.java
@@ -8,4 +8,13 @@ public class AppUtilities {
     public static final String POSTER_FILM = "https://image.tmdb.org/t/p/w185/";
     public static final String TAG = "tag";
 
+    /*pencarian film.
+    Movies: https://api.themoviedb.org/3/search/movie?api_key={API KEY}&language=en-US&query={MOVIE NAME}
+    Tv Show: https://api.themoviedb.org/3/search/tv?api_key={API KEY}&language=en-US&query={TV SHOW NAME}
+    Contoh: https://api.themoviedb.org/3/search/movie?api_key=123456789&language=en-US&query=Avenger*/
+
+    /*film yang rilis pada tanggal hari ini.
+    Movies release: https://api.themoviedb.org/3/discover/movie?api_key={API KEY}&primary_release_date.gte={TODAY DATE}&primary_release_date.lte={TODAY DATE}
+    Contoh: https://api.themoviedb.org/3/discover/movie?api_key=123456789&primary_release_date.gte=2019-01-31&primary_release_date.lte=2019-01-31
+    Catatan: Pastikan format tanggal yang kalian gunakan benar. Format tanggal yang digunakan adalah "yyyy-MM-dd".*/
 }
