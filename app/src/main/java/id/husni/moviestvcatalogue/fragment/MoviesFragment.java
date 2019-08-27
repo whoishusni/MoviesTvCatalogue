df063ecbfedf29db28a3b2fa9466e9e9d6f9c57f
Husni <mohhusnimubaraq@gmail.com>
2019-08-27 17:44:15 +0700

    Update 5 (Movies Data Is Shown + minor change)

--- app/src/main/java/id/husni/moviestvcatalogue/fragment/MoviesFragment.java
+++ app/src/main/java/id/husni/moviestvcatalogue/fragment/MoviesFragment.java
@@ -5,17 +5,29 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ProgressBar;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProvider;
+import androidx.lifecycle.ViewModelProviders;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
 
 import id.husni.moviestvcatalogue.R;
+import id.husni.moviestvcatalogue.adapter.MoviesAdapter;
+import id.husni.moviestvcatalogue.model.Movies;
+import id.husni.moviestvcatalogue.viewmodel.MoviesViewModel;
 
-
-/**
- * A simple {@link Fragment} subclass.
- */
 public class MoviesFragment extends Fragment {
-
+    MoviesAdapter adapter;
+    MoviesViewModel model;
+    RecyclerView recyclerView;
+    ProgressBar progressBar;
 
     public MoviesFragment() {
         // Required empty public constructor
@@ -25,8 +37,41 @@ public class MoviesFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_movies, container, false);
     }
 
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        adapter = new MoviesAdapter(getContext());
+
+        progressBar = view.findViewById(R.id.progressBarMovies);
+
+        recyclerView = view.findViewById(R.id.recyclerMovies);
+        recyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
+        recyclerView.setAdapter(adapter);
+
+        model = ViewModelProviders.of(this).get(MoviesViewModel.class);
+        model.setMoviesData();
+        showLoading(true);
+        model.getMoviesData().observe(this, moviesObserver);
+
+    }
+
+    Observer<ArrayList<Movies>> moviesObserver = new Observer<ArrayList<Movies>>() {
+        @Override
+        public void onChanged(ArrayList<Movies> movies) {
+            adapter.setMovies(movies);
+            showLoading(false);
+        }
+    };
+
+    private void showLoading(boolean loading) {
+        if (loading) {
+            progressBar.setVisibility(View.VISIBLE);
+        } else {
+            progressBar.setVisibility(View.GONE);
+        }
+    }
 }
---
+++ app/src/main/java/id/husni/moviestvcatalogue/viewmodel/MoviesViewModel.java
@@ -0,0 +1,59 @@
+package id.husni.moviestvcatalogue.viewmodel;
+
+import android.util.Log;
+
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.ViewModel;
+
+import com.loopj.android.http.AsyncHttpClient;
+import com.loopj.android.http.AsyncHttpResponseHandler;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+
+import cz.msebera.android.httpclient.Header;
+import id.husni.moviestvcatalogue.model.Movies;
+import id.husni.moviestvcatalogue.utilities.AppUtilities;
+
+public class MoviesViewModel extends ViewModel {
+
+    MutableLiveData<ArrayList<Movies>> mutableMovies = new MutableLiveData<>();
+
+    public void setMoviesData() {
+        final ArrayList<Movies> moviesArrayList = new ArrayList<>();
+        AsyncHttpClient client = new AsyncHttpClient();
+        client.get(AppUtilities.URL_MOVIES_TMDB, new AsyncHttpResponseHandler() {
+            @Override
+            public void onSuccess(int statusCode, Header[] headers, byte[] responseBody) {
+                try {
+                    String result = new String(responseBody);
+                    JSONObject object = new JSONObject(result);
+                    JSONArray moviesArray = object.getJSONArray("results");
+
+                    for (int i = 0; i < moviesArray.length(); i++) {
+                        JSONObject moviesObject = moviesArray.getJSONObject(i);
+                        Movies movies = new Movies(moviesObject);
+                        moviesArrayList.add(movies);
+                        Log.d(AppUtilities.TAG, "onSuccess: catch movies data view model");
+                    }
+                    mutableMovies.postValue(moviesArrayList);
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+            }
+
+            @Override
+            public void onFailure(int statusCode, Header[] headers, byte[] responseBody, Throwable error) {
+                Log.d(AppUtilities.TAG, "onFailure: catch movies data view model ");
+            }
+        });
+    }
+
+    public LiveData<ArrayList<Movies>> getMoviesData() {
+        return mutableMovies;
+    }
+}
